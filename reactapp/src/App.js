78ca15714539e9ef862be99c65f61c7f9368adf0
Abhis113 <abhishekranjangecd@gmail.com>
2022-10-18 18:18:02 +0530

    portfolio

---
+++ reactapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import navbar from "./Component/navbar";
+function App() {
+  return (
+    <>
+       Hello react
+       <navbar />
+
+    </>
+  );
+}
+
+export default App;
